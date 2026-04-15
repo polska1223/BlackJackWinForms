b3a97bf3ada0d4d4694940aee775f0f3190035b5
MD-DD <Mikolajdanicki34@gmail.com>
2026-04-15 12:06:35 +0200

    added player class with score calculation

--- BlackJackWinforms/models/Player.cs
+++ BlackJackWinforms/models/Player.cs
@@ -1,10 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace BlackJackWInforms.models
+namespace BlackJackWinforms
 {
-    internal class Player
+    public class Player
     {
+        public string Name { get; set; }
+        public List<Card> Hand { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Hand = new List<Card>();
+        }
+
+        public void AddCard(Card card)
+        {
+            Hand.Add(card);
+        }
+
+        public int CalculateScore()
+        {
+            int score = 0;
+            int aceCount = 0;
+
+            foreach (Card card in Hand)
+            {
+                score += card.GetValue();
+
+                if (card.Rank == "Ace")
+                {
+                    aceCount++;
+                }
+            }
+
+            while (score > 21 && aceCount > 0)
+            {
+                score -= 10;
+                aceCount--;
+            }
+
+            return score;
+        }
+
+        public bool IsBust()
+        {
+            return CalculateScore() > 21;
+        }
+
+        public bool HasBlackjack()
+        {
+            return Hand.Count == 2 && CalculateScore() == 21;
+        }
     }
 }
