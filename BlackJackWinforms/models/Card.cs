2f4ba6d77d41649a7c8d5f34bc26219307fecffd
MD-DD <Mikolajdanicki34@gmail.com>
2026-04-15 11:23:54 +0200

    added card class for blackjack & added needed models

---
+++ BlackJackWinforms/models/Card.cs
@@ -0,0 +1,34 @@
+﻿namespace BlackJackWinforms
+{
+    public class Card
+    {
+        public string Suit { get; set; }
+        public string Rank { get; set; }
+
+        public Card(string suit, string rank)
+        {
+            Suit = suit;
+            Rank = rank;
+        }
+
+        public int GetValue()
+        {
+            if (Rank == "Jack" || Rank == "Queen" || Rank == "King")
+            {
+                return 10;
+            }
+
+            if (Rank == "Ace")
+            {
+                return 11;
+            }
+
+            return int.Parse(Rank);
+        }
+
+        public override string ToString()
+        {
+            return $"{Rank} of {Suit}";
+        }
+    }
+}
---
+++ BlackJackWinforms/models/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJackWInforms.models
+{
+    internal class GameManager
+    {
+    }
+}
