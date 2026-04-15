1c966044913cc6e2c201d453e31651b2f8deabbe
MD-DD <Mikolajdanicki34@gmail.com>
2026-04-15 11:55:00 +0200

    added deck class with 52 cards

--- BlackJackWinforms/models/Deck.cs
+++ BlackJackWinforms/models/Deck.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace BlackJackWInforms.models
+namespace BlackJackWinforms
 {
-    internal class deck
+    public class Deck
     {
+        public List<Card> Cards { get; set; }
+
+        public Deck()
+        {
+            Cards = new List<Card>();
+
+            string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+            string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+
+            foreach (string suit in suits)
+            {
+                foreach (string rank in ranks)
+                {
+                    Cards.Add(new Card(suit, rank));
+                }
+            }
+        }
     }
 }
