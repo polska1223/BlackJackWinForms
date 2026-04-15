169e4e8190f2d55b56ed765281d6b96c55ed3e01
MD-DD <Mikolajdanicki34@gmail.com>
2026-04-15 11:57:52 +0200

    added shoe class with shuffle and draw

--- BlackJackWinforms/models/Shoe.cs
+++ BlackJackWinforms/models/Shoe.cs
@@ -1,10 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
-namespace BlackJackWInforms.models
+namespace BlackJackWinforms
 {
-    internal class Shoe
+    public class Shoe
     {
+        private List<Card> cards;
+        private Random random;
+
+        public Shoe(int numberOfDecks)
+        {
+            cards = new List<Card>();
+            random = new Random();
+
+            for (int i = 0; i < numberOfDecks; i++)
+            {
+                Deck deck = new Deck();
+                cards.AddRange(deck.Cards);
+            }
+
+            Shuffle();
+        }
+
+        public void Shuffle()
+        {
+            for (int i = cards.Count - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                Card temp = cards[i];
+                cards[i] = cards[j];
+                cards[j] = temp;
+            }
+        }
+
+        public Card DrawCard()
+        {
+            Card card = cards[0];
+            cards.RemoveAt(0);
+            return card;
+        }
     }
 }
